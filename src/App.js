50db6966a5d9a599a2960af8e0abfb122dba92fb
n3rdgir1 <nerdgirl11235813@gmail.com>
2017-09-27 23:00:52 -0500

    switches out the default app for framework

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, NavItem } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+  handleSelect(selectedKey) {
+    alert(selectedKey)
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Started a project!
-        </p>
+      <div className="main">
+        <Navbar bsStyle="pills" activeKey={1} onSelect={this.handleSelect}>
+          <NavItem eventKey={1} href="/home">NavItem 1 content</NavItem>
+          <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
+          <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
+        </Navbar>
       </div>
     );
   }
