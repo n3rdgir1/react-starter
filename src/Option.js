9e323ff094a82ec70944ca35298e1cf13bf357b6
n3rdgir1 <nerdgirl11235813@gmail.com>
2018-03-28 18:49:53 -0500

    highlights correct answer and refreshes

--- src/Option.js
+++ src/Option.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class Option extends Component {
+  className() {
+    let classString = 'card col-6 '
+    if(this.state) classString += this.state.correct ? 'correct' : 'incorrect';
+    return classString;
+  }
+
+  kana() {
+    return this.props.character[this.props.type];
+  }
+
+  handleClick(e) {
+    if(!this.state) {
+      this.setState({correct: this.props.answer})
+    } else if (this.state.correct) {
+      window.location.reload();
+    }
+  }
+
   render() {
     return (
-      <div className={ this.props.answer ? 'answer card col-6' : 'card col-6' }>
-        {this.props.character[this.props.type]}
+      <div className={ this.className.bind(this)() } onClick={ this.handleClick.bind(this)}>
+        {this.kana.bind(this)()}
       </div>
     );
   }
