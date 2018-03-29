a1b9ab004094ae559d275526c23f86a5b2efe12a
n3rdgir1 <nerdgirl11235813@gmail.com>
2018-03-28 19:00:18 -0500

    updates options to be random

--- src/Kana.js
+++ src/Kana.js
@@ -12,27 +12,41 @@ class Kana extends Component {
     return Math.floor(Math.random() * characters.length)
   }
 
-  options(answerIndex) {
-    return <div className='answers'>
-      <div className='row'>
-        <Option character={characters[this.random()]} type={this.props.type} />
-        <Option character={characters[this.random()]} type={this.props.type} />
-      </div>
-      <div className='row'>
-        <Option character={this.answer(answerIndex)} answer={true} type={this.props.type} />
-        <Option character={characters[this.random()]} type={this.props.type} />
-      </div>
-    </div>
+  shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  randomOptions(answerIndex) {
+    return this.shuffle([
+      {kana: characters[this.random()], answer: false},
+      {kana: characters[this.random()], answer: false},
+      {kana: characters[this.random()], answer: false},
+      {kana: this.answer(answerIndex), answer: true}
+    ]);
   }
 
   render() {
-    var answerIndex = this.random()
+    const answerIndex = this.random()
+    const options = this.randomOptions(answerIndex)
     return (
       <div>
         <div className='question'>
           What is the <span className='type'>{this.props.type}</span> of <span className='romanji'>{this.answer(answerIndex).romanji}</span>?
         </div>
-        {this.options(answerIndex)}
+        <div className='answers'>
+          <div className='row'>
+            <Option character={options[0].kana} answer={options[0].answer} type={this.props.type} />
+            <Option character={options[1].kana} answer={options[1].answer} type={this.props.type} />
+          </div>
+          <div className='row'>
+            <Option character={options[2].kana} answer={options[2].answer} type={this.props.type} />
+            <Option character={options[3].kana} answer={options[3].answer} type={this.props.type} />
+          </div>
+        </div>
       </div>
     );
   }
